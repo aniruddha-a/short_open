801f5c2e0cd9c2c999dad59bf6fd719016f7b10d
Aniruddha. A <ani@versa-networks.com>
2013-05-16 00:40:46 -0700

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-short_open
+Short Open
 ==========
 
-This is a simple perl script, which, when combined with shell alias, can be very helpful to quickly edit/open a file (from the command line)
+Most of the source files that I work on, follow this `long_underscore_separated_names` convention, 
+and I hate to press _TAB TAB TAB_ ...
+When I know a file name, or am seeing it on console (with a previous `ls`), 
+I want to be able to open a file (in the current directory) with as few keystrokes as possible.
+
+I came up with this mini Perl script, which, when combined with shell aliases
+can be a real time saver. 
+The same can be achieved with sed/awk or any other regex-well-supported
+languages, but Perl is my choice.
+
+Aliases
+=======
+
+Here are a few `bash` aliases that I use (which specify the editor I want to use,
+and the file extension):
+
+        alias vc='~/short_open.pl vim c' 
+        alias vh='~/short_open.pl vim h'
+        alias vp='~/short_open.pl vim p[ly]'
+        alias vy='~/short_open.pl vim yml'
+        alias vd='~/short_open.pl vim diff'
+        alias vs='~/short_open.pl vim sh'
+        alias vl='~/short_open.pl vim l[ou][ga]'
+
+Given a file like: `my_lisp_parser.c`, I can edit it, in `vim` with 
+        
+        $ vc mlp
+        or
+        $ vc m l p
+
+i.e, the first letters of the word. 
+In case of ambiguity, say I had another file `my_lex_parser.c` (hypothetical),
+can run:
+
+        $ vc m le p
+
+With a small change in the regex, it can be made to 
+work on _CamelCase_ file names as well.
+
---
+++ short_open.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl -w
+#  Aniruddha.A (aniruddha.a@gmail.com)
+#  Given abbreviated name  - expand and edit the file
+#     underscore/hyphen as seperator
+#
+#     Usage: <script> <extension> <first-letters>
+#               Example: short_open.pl  c  mlp  --> will open  my_lisp_parser.c
+#
+#            <script> <extension> <first-few-letters> [ <first-few-letters> ...]
+#               Example: short_open.pl  h  m list p  --> will open  my_list_parser.h
+
+$editor = shift;
+$ext = shift;
+
+if ( scalar(@ARGV) == 1 ) {
+    # A few special cases - which I commonly edit
+    $in = shift; 
+    if ((length($in) == 1) and ($in eq 'm')) {
+        system("$editor [Mm]akefile");
+        exit 0;
+    }
+    if ((length($in) == 1) and ($in eq 'R')) {
+        system("$editor README");
+        exit 0;
+    }
+    if ((length($in) == 1) and ($in eq 'w')) {
+        system("$editor wscript");
+        exit 0;
+    }
+    @p = split( //, $in ); # 1 arg - pick and split each char
+}
+else {
+    @p = (@ARGV); # multi arg - use them as is
+}
+$res = '^';
+$res .= join( '[^_-]*[-_]', @p );
+$res .= '[^_-]*[.]';
+$res .= $ext . '$';  
+$re = qr/$res/;
+foreach $a ( glob("*.$ext") ) {
+    if ( $a =~ m/$re/ ) {
+        print " [$a] \n";
+        system ("$editor $a");
+        exit 0;
+    }
+}
+
+# If it were not really a long name with underscore, then try
+# a plain short name (assume 1 arg)
+if ( scalar(@ARGV) > 1 ) {
+    exit 1;
+}
+$res = $in;   
+$res .= '.*[.]';  #  match any part of the file name 
+$res .= $ext;     
+$re = qr/$res/;
+foreach $a ( glob("*.$ext") ) {
+    if ( $a =~ m/$re/ ) {
+        print " [$a] \n";
+        system ("$editor $a");
+        exit 0;
+    }
+}
+
